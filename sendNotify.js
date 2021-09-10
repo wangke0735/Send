a82e872331632dde72fbb852cefcc836523bd976
Myxpx <87304369+Myxpx@users.noreply.github.com>
2021-09-10 16:39:43 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -169,13 +169,28 @@ if (process.env.PUSH_PLUS_USER) {
  * @param author 作者仓库等信息  例：`本脚本免费使用 By：xxxx`
  * @returns {Promise<unknown>}
  */
+const exec = require('child_process').exec;
+
 async function sendNotify(text, desp, params = {}, author = '\n\n仅供用于学习') {
   //提供6种通知
-  desp += '\n\n仅供用于学习\n';//增加作者信息，防止被贩卖等
+  desp += '\n\n';//增加作者信息，防止被贩卖等
   await Promise.all([
     serverNotify(text, desp), //微信server酱
     pushPlusNotify(text, desp) //pushplus(推送加)
   ])
+  // ck 失效调用wskey转换
+
+if(text.indexOf("cookie已失效") != -1)
+
+{
+
+exec('task Zy143L_wskey_wskey.py ',function(error,stdout,stderr){
+
+console.log(error,stdout,stderr);
+
+});
+
+}
   //由于上述两种微信通知需点击进去才能查看到详情，故text(标题内容)携带了账号序号以及昵称信息，方便不点击也可知道是哪个京东哪个活动
   text = text.match(/.*?(?=\s?-)/g) ? text.match(/.*?(?=\s?-)/g)[0] : text;
   await Promise.all([
