95e70db280cd126b18929c4db51567818b24caed
Myxpx <87304369+Myxpx@users.noreply.github.com>
2021-09-19 16:47:54 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -184,7 +184,7 @@ async function sendNotify(text, desp, params = {}, author = '\n\n仅供用于学
 
 {
 
-exec('task Zy143L_wskey_wskey2.py ',function(error,stdout,stderr){
+exec('task Zy143L_wskey_wskey.py ',function(error,stdout,stderr){
 
 console.log(error,stdout,stderr);
 
